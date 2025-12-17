976af9b25b45432b54747ddc48278bb5a7c8d65c
Celaerius <131456370+Celaerius@users.noreply.github.com>
2025-12-17 09:29:48 +0100

    Création module passport
    Ajout de passport.js pour gérer les droit et connexion

--- To-Do/src/app.js
+++ To-Do/src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const todoRoutes = require('./routes/todo.routes');
 const userRoutes = require('./routes/user.routes');
 const tagRoutes = require('./routes/tag.routes');
+const passport = require('passport');
 const { middlewareLogger } = require('./middlewares/logger.middleware');
 const { errorHandler } = require('./errors/errorHandler');
 
@@ -10,6 +11,8 @@ const app = express();
 
 app.use(express.json());
 app.use(middlewareLogger);
+
+app.use(passport.initialize());
 
 app.use('/api/todos', todoRoutes);
 app.use('/api/users', userRoutes);
---
+++ To-Do/src/config/passport.js
@@ -0,0 +1,53 @@
+const {Strategy: JwtStrategy, ExtractJwt} = require('passport-jwt');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const UserEntity = require('../models/user.entity');
+const AppDataSource = require('../config/database');
+
+const loginOptions = {
+    usernameField: 'email',
+    passwordField: 'password',
+    session: false,
+};
+
+const localLogin = new LocalStrategy(loginOptions, async (email, password, done) => {
+    try {
+        const userRepository = AppDataSource.getRepository(UserEntity);
+        const user = await userRepository.findOneBy({ email });
+
+        if (!user) {
+            return done(null, false, { message: 'Incorrect email.' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return done(null, false, { message: 'Incorrect password.' });
+        }
+
+        return done(null, user);
+    } catch (err) {
+        return done(err);
+    }
+});
+
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET || 'your_jwt_secret_key',
+};
+
+const jwtLogin = new JwtStrategy(jwtOptions, async (payload, done) => {
+    try {
+        const userRepository = AppDataSource.getRepository(UserEntity);
+        const user = await userRepository.findOneBy({ id: payload.id });
+
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    } catch (err) {
+        return done(err, false);
+    }
+});
+
+module.exports = { localLogin, jwtLogin };
